7da9df1ff13995c62d404090c2097b9ccb45d129
nielsbischoff <niels.bischoff@gmail.com>
2015-07-08 18:54:09 -0700

    adds the post.js collection

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,1 +1,1 @@
-Template.postItem.helpers({ domain: function() {var a = document.createElement('a'); a.href = this.url;return a.hostname;} });
+Template.postItem.helpers({ domain: function() {    var a = document.createElement('a'); a.href = this.url;    return a.hostname;} });
